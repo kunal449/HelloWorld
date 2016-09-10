9628051edd57541ee78e7b31eef417270dd60d55
kunal449 <kunal44mhl@gmail.com>
2016-09-10 14:50:44 +0530

    adding changes to README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # HelloWorld
+Date : 10 Sept 2016.
+
+Hello Git. The very First Java Program on git.
+Trying out commands on Git.
--- src/com/personal/kunal/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package com.peronal.kunal;
-
-public class HelloWorld {
-	public static void main(String[] args){
-		System.out.println("Hello Git World.");
-	}
-}
